e4e899dc70f4be07276c9c08d809f821d6fbd586
Chris Ware <c.p.ware@hotmail.com>
2024-02-07 17:40:03 -0700

    Added Next Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
+import Providers from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,12 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <div className=' flex flex-col'>
-          <Header />
-        </div>
-        {children}
+        <Providers>
+          <div className=' flex flex-col'>
+            <Header />
+          </div>
+          {children}
+        </Providers>
       </body>
     </html>
   );
